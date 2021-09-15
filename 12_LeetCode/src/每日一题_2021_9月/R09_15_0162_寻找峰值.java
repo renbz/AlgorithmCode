879fcd03966750a9f13a6ead79d1b35dd416be00
renbz <1362206371@qq.com>
2021-09-15 23:39:12 +0800

    算法代码更新提交

--- 12_LeetCode/src/每日一题_2021_9月/R09_15_0162_寻找峰值.java
+++ 12_LeetCode/src/每日一题_2021_9月/R09_15_0162_寻找峰值.java
@@ -7,8 +7,16 @@ package 每日一题_2021_9月;
 public class R09_15_0162_寻找峰值 {
 
     public int findPeakElement(int[] nums) {
-        for (int i = 1; i < nums.length-1; i++) {
-            if(nums[i]>nums[i-1] && nums[i]<nums[i+1]) return i;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            boolean ok = true;
+            if (i - 1 >= 0) {
+                if (nums[i - 1] >= nums[i]) ok = false;
+            }
+            if (i + 1 < n) {
+                if (nums[i + 1] >= nums[i]) ok = false;
+            }
+            if (ok) return i;
         }
         return -1;
     }
