d706cfa4d3cf16d7471156947358af16c3717eae
renbz <1362206371@qq.com>
2021-09-17 14:00:53 +0800

    算法代码更新提交

--- 07_企业笔试题/src/main/java/字节跳动/R_2019春招_problems2/R01_万万没想到之聪明的编辑.java
+++ 07_企业笔试题/src/main/java/字节跳动/R_2019春招_problems2/R01_万万没想到之聪明的编辑.java
@@ -1,6 +1,5 @@
 package 字节跳动.R_2019春招_problems2;
 
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,24 +10,23 @@ public class R01_万万没想到之聪明的编辑 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         int k = in.nextInt();
-        while(k-- > 0){
+        while (k-- > 0) {
             String str = in.next();
             char[] c = str.toCharArray();
             ArrayList<Character> list = new ArrayList<>();
             for (int i = 0; i < c.length; i++) {
                 list.add(c[i]);
             }
-            for (int i = 0; i < list.size()-2; i++) {
-                if(list.get(i).equals(list.get(i+1)) && list.get(i).equals(list.get(i+2))){
+            for (int i = 0; i < list.size() - 2; i++) {
+                if (list.get(i).equals(list.get(i + 1)) && list.get(i).equals(list.get(i + 2))) {
                     list.remove(i);
-                    i=0;
+                    i = 0;
                 }
-                if(i<list.size()-3){
-                    if(list.get(i).equals(list.get(i+1)) && list.get(i+2).equals(list.get(i+3))){
-                        list.remove(i+2);
-                        i=0;
+                if (i < list.size() - 3) {
+                    if (list.get(i).equals(list.get(i + 1)) && list.get(i + 2).equals(list.get(i + 3))) {
+                        list.remove(i + 2);
+                        i = 0;
                     }
                 }
             }
@@ -39,6 +37,4 @@ public class R01_万万没想到之聪明的编辑 {
             System.out.println(sb.toString());
         }
     }
-
-
 }
