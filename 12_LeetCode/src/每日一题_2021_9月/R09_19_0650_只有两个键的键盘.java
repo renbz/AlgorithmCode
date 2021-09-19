306b208e732ae4b26ad29d1652d56df6c85fb122
renbz <1362206371@qq.com>
2021-09-19 20:41:46 +0800

    算法代码更新提交

---
+++ 12_LeetCode/src/每日一题_2021_9月/R09_19_0650_只有两个键的键盘.java
@@ -0,0 +1,21 @@
+package 每日一题_2021_9月;
+
+/**
+ * @author Ren
+ */
+
+public class R09_19_0650_只有两个键的键盘 {
+    public int minSteps(int n) {
+        int[] f = new int[n + 1];
+        for (int i = 2; i <= n; ++i) {
+            f[i] = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; ++j) {
+                if (i % j == 0) {
+                    f[i] = Math.min(f[i], f[j] + i / j);
+                    f[i] = Math.min(f[i], f[i / j] + j);
+                }
+            }
+        }
+        return f[n];
+    }
+}
