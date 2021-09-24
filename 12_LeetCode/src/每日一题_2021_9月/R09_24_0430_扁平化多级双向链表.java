c6f8c7c25bbafb1c924bbe6eea25df457bb8e753
renbz <1362206371@qq.com>
2021-09-24 17:43:58 +0800

    算法代码更新提交

--- 12_LeetCode/src/每日一题_2021_9月/R09_24_0430_扁平化多级双向链表.java
+++ 12_LeetCode/src/每日一题_2021_9月/R09_24_0430_扁平化多级双向链表.java
@@ -7,8 +7,41 @@ package 每日一题_2021_9月;
 public class R09_24_0430_扁平化多级双向链表 {
 
     public Node flatten(Node head) {
+        dfs(head);
+        return head;
+    }
 
-        return null;
+    public Node dfs(Node node) {
+        Node cur = node;
+        // 记录链表的最后一个节点
+        Node last = null;
+
+        while (cur != null) {
+            Node next = cur.next;
+            //  如果有子节点，那么首先处理子节点
+            if (cur.child != null) {
+                Node childLast = dfs(cur.child);
+
+                next = cur.next;
+                //  将 node 与 child 相连
+                cur.next = cur.child;
+                cur.child.prev = cur;
+
+                //  如果 next 不为空，就将 last 与 next 相连
+                if (next != null) {
+                    childLast.next = next;
+                    next.prev = childLast;
+                }
+
+                // 将 child 置为空
+                cur.child = null;
+                last = childLast;
+            } else {
+                last = cur;
+            }
+            cur = next;
+        }
+        return last;
     }
 
 
