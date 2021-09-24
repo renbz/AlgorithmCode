db836a53196eb3003a1f6b9e6c0e0dd3cb84922e
renbz <1362206371@qq.com>
2021-09-24 15:17:56 +0800

    算法代码更新提交

---
+++ 12_LeetCode/src/每日一题_2021_9月/R09_24_0430_扁平化多级双向链表.java
@@ -0,0 +1,22 @@
+package 每日一题_2021_9月;
+
+/**
+ * @author Ren
+ */
+
+public class R09_24_0430_扁平化多级双向链表 {
+
+    public Node flatten(Node head) {
+
+        return null;
+    }
+
+
+    class Node {
+        public int val;
+        public Node prev;
+        public Node next;
+        public Node child;
+    }
+
+}
