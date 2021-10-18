36a7d7fc135a2980fcbe60a443ed25736ff9cd45
renbz <1362206371@qq.com>
2021-10-18 09:17:14 +0800

    算法代码更新提交

--- 13_Tmp/src/main/java/other/GenericDefinitionDemo.java
+++ 13_Tmp/src/main/java/other/GenericDefinitionDemo.java
@@ -9,11 +9,13 @@ public class GenericDefinitionDemo<T> {
     static <String,T,Xiaomi> String get(String string,Xiaomi xiaomi){
         return string;
     }
-
+    static <A,B> A get2(A a,B b){
+        return a;
+    }
     public static void main(String[] args) {
         Integer first = 222;
         Long second = 333L;
-        // 调用上方定义的get方法
+        // 调用上方定义的get方法zl
         Integer result = get(first,second);
         System.out.println(result);
     }
