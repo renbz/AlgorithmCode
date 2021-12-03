d53ca89a76f2c87df9658f748c300238ca5bd0f8
renbz <1362206371@qq.com>
2021-12-03 19:39:02 +0800

    每日一题  12-03

---
+++ 12_LeetCode/src/每日一题_2021_12月/R12_04_0383_赎金信.java
@@ -0,0 +1,31 @@
+package 每日一题_2021_12月;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2021/12/3 17:04
+ * @Description:
+ */
+
+public class R12_04_0383_赎金信 {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> RanMap = new HashMap<>();
+        Map<Character, Integer> MagMap = new HashMap<>();
+        for (int i = 0; i < ransomNote.length(); i++)
+            RanMap.put(ransomNote.charAt(i), RanMap.getOrDefault(ransomNote.charAt(i), 0)+1);
+        for (int i = 0; i < magazine.length(); i++)
+            MagMap.put(magazine.charAt(i), MagMap.getOrDefault(magazine.charAt(i), 0)+1);
+        Set<Map.Entry<Character, Integer>> entries = RanMap.entrySet();
+        for (Map.Entry<Character, Integer> entry : entries) {
+            char key = entry.getKey();
+            int value = entry.getValue();
+            if (value > MagMap.getOrDefault(key,0))
+                return false;
+        }
+        return true;
+    }
+}
