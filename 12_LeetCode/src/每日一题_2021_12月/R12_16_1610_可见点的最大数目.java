968fa84c912020c9f2b5d61dc3247595801e1608
renbz <1362206371@qq.com>
2021-12-16 20:59:47 +0800

    每日一题  12-16  1610 可见点的最大数目

---
+++ 12_LeetCode/src/每日一题_2021_12月/R12_16_1610_可见点的最大数目.java
@@ -0,0 +1,51 @@
+package 每日一题_2021_12月;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2021/12/16 09:35
+ * @Description:
+ */
+
+public class R12_16_1610_可见点的最大数目 {
+
+    public int visiblePoints(List<List<Integer>> points, int angle, List<Integer> location) {
+        int sameCnt = 0;
+        List<Double> polarDegrees = new ArrayList<>();
+        int locationX = location.get(0);
+        int locationY = location.get(1);
+        for (int i = 0; i < points.size(); ++i) {
+            int x = points.get(i).get(0);
+            int y = points.get(i).get(1);
+            if (x == locationX && y == locationY) {
+                sameCnt++;
+                continue;
+            }
+            Double degree = Math.atan2(y - locationY, x - locationX);
+            polarDegrees.add(degree);
+        }
+        Collections.sort(polarDegrees);
+
+        int m = polarDegrees.size();
+        for (int i = 0; i < m; ++i) {
+            polarDegrees.add(polarDegrees.get(i) + 2 * Math.PI);
+        }
+
+        int maxCnt = 0;
+        int right = 0;
+        double toDegree = angle * Math.PI / 180;
+        for (int i = 0; i < m; ++i) {
+            Double curr = polarDegrees.get(i) + toDegree;
+            while (right < polarDegrees.size() && polarDegrees.get(right) <= curr) {
+                right++;
+            }
+            maxCnt = Math.max(maxCnt, right - i);
+        }
+        return maxCnt + sameCnt;
+    }
+
+}
