fbedee25784a539683616d7b356b085f145a607e
renbz <1362206371@qq.com>
2021-12-18 10:17:33 +0800

    每日一题  12-18 0419 甲板上的战舰

---
+++ 12_LeetCode/src/每日一题_2021_12月/R12_18_0419_甲板上的战舰.java
@@ -0,0 +1,32 @@
+package 每日一题_2021_12月;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2021/12/18 10:11
+ * @Description:
+ */
+
+public class R12_18_0419_甲板上的战舰 {
+
+    public int countBattleships(char[][] board) {
+        int row = board.length;
+        int col = board[0].length;
+        int ans = 0;
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (board[i][j] == 'X') {
+                    board[i][j] = '.';
+                    for (int k = j + 1; k < col && board[i][k] == 'X'; ++k) {
+                        board[i][k] = '.';
+                    }
+                    for (int k = i + 1; k < row && board[k][j] == 'X'; ++k) {
+                        board[k][j] = '.';
+                    }
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+}
