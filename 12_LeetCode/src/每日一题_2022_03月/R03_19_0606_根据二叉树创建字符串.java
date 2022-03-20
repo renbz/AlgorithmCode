36c46d9fca434b56c4bf02e0785e72bdba06e4d1
renbz <1362206371@qq.com>
2022-03-20 15:42:28 +0800

    每日一题  03_19_0606_根据二叉树创建字符串

---
+++ 12_LeetCode/src/每日一题_2022_03月/R03_19_0606_根据二叉树创建字符串.java
@@ -0,0 +1,58 @@
+package 每日一题_2022_03月;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/3/19 18:12
+ * @Description:
+ */
+
+public class R03_19_0606_根据二叉树创建字符串 {
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    public String tree2str(TreeNode root) {
+        if (root == null) {
+            return "";
+        }
+        if (root.left == null && root.right == null) {
+            return Integer.toString(root.val);
+        }
+        if (root.right == null) {
+            return new StringBuffer().append(root.val).append("(").append(tree2str(root.left)).append(")").toString();
+        }
+        return new StringBuffer().append(root.val).append("(").append(tree2str(root.left)).append(")(").append(tree2str(root.right)).append(")").toString();
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
