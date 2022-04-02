fbc3aadb755b87765626b15d2c60cf0a1f95c77f
renbz <1362206371@qq.com>
2022-04-02 17:29:52 +0800

    每日一题  04_01_0954_二倍数对数组

---
+++ 12_LeetCode/src/学习计划/编程能力/编程能力入门/Day08_09_字符串/R0709_转换成小写字母.java
@@ -0,0 +1,15 @@
+package 学习计划.编程能力.编程能力入门.Day08_09_字符串;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/3/31 21:02
+ * @Description:
+ */
+
+public class R0709_转换成小写字母 {
+
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+}
