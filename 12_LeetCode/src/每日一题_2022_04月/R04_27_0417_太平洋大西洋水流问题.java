87db6c8b0e9f9e9649384da9d439c09669eedadd
renbz <1362206371@qq.com>
2022-04-28 12:41:44 +0800

    每日一题  04_27_0417_太平洋大西洋水流问题

---
+++ 12_LeetCode/src/每日一题_2022_04月/R04_27_0417_太平洋大西洋水流问题.java
@@ -0,0 +1,63 @@
+package 每日一题_2022_04月;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/4/27 23:01
+ * @Description:
+ */
+
+public class R04_27_0417_太平洋大西洋水流问题 {
+
+    static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int[][] heights;
+    int m, n;
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        this.heights = heights;
+        this.m = heights.length;
+        this.n = heights[0].length;
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            dfs(i, 0, pacific);
+        }
+        for (int j = 1; j < n; j++) {
+            dfs(0, j, pacific);
+        }
+        for (int i = 0; i < m; i++) {
+            dfs(i, n - 1, atlantic);
+        }
+        for (int j = 0; j < n - 1; j++) {
+            dfs(m - 1, j, atlantic);
+        }
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    List<Integer> cell = new ArrayList<Integer>();
+                    cell.add(i);
+                    cell.add(j);
+                    result.add(cell);
+                }
+            }
+        }
+        return result;
+    }
+
+    public void dfs(int row, int col, boolean[][] ocean) {
+        if (ocean[row][col]) {
+            return;
+        }
+        ocean[row][col] = true;
+        for (int[] dir : dirs) {
+            int newRow = row + dir[0], newCol = col + dir[1];
+            if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && heights[newRow][newCol] >= heights[row][col]) {
+                dfs(newRow, newCol, ocean);
+            }
+        }
+    }
+
+}
