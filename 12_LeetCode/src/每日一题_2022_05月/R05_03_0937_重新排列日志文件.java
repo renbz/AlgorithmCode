703a54701e9870a053563eb45f1f9b013386802b
renbz <1362206371@qq.com>
2022-05-04 17:51:24 +0800

    每日一题  05_03_0937_重新排列日志文件

---
+++ 12_LeetCode/src/每日一题_2022_05月/R05_03_0937_重新排列日志文件.java
@@ -0,0 +1,56 @@
+package 每日一题_2022_05月;
+
+import java.util.Arrays;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/5/3 15:32
+ * @Description:
+ */
+
+public class R05_03_0937_重新排列日志文件 {
+
+    public String[] reorderLogFiles(String[] logs) {
+        int length = logs.length;
+        Pair[] arr = new Pair[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = new Pair(logs[i], i);
+        }
+        Arrays.sort(arr, (a, b) -> logCompare(a, b));
+        String[] reordered = new String[length];
+        for (int i = 0; i < length; i++) {
+            reordered[i] = arr[i].log;
+        }
+        return reordered;
+    }
+
+    public int logCompare(Pair pair1, Pair pair2) {
+        String log1 = pair1.log, log2 = pair2.log;
+        int index1 = pair1.index, index2 = pair2.index;
+        String[] split1 = log1.split(" ", 2);
+        String[] split2 = log2.split(" ", 2);
+        boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+        boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+        if (isDigit1 && isDigit2) {
+            return index1 - index2;
+        }
+        if (!isDigit1 && !isDigit2) {
+            int sc = split1[1].compareTo(split2[1]);
+            if (sc != 0) {
+                return sc;
+            }
+            return split1[0].compareTo(split2[0]);
+        }
+        return isDigit1 ? 1 : -1;
+    }
+
+    class Pair {
+        String log;
+        int index;
+
+        public Pair(String log, int index) {
+            this.log = log;
+            this.index = index;
+        }
+    }
+}
