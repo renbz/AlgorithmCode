482564aa3926aabb793babac01cca53d94b0bc53
renbz <1362206371@qq.com>
2022-05-17 00:00:08 +0800

    每日一题  05_16_面试题_04_06_后继者

---
+++ 12_LeetCode/src/每日一题_2022_05月/R05_16_面试题_04_06_后继者.java
@@ -0,0 +1,42 @@
+package 每日一题_2022_05月;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/5/16 23:54
+ * @Description:
+ */
+
+public class R05_16_面试题_04_06_后继者 {
+
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+        TreeNode prev = null, curr = root;
+        while (!stack.isEmpty() || curr != null) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            if (prev == p) {
+                return curr;
+            }
+            prev = curr;
+            curr = curr.right;
+        }
+        return null;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
