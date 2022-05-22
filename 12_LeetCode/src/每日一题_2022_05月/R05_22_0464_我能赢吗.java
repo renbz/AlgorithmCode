25dc0716ceb91d8efaf516f935c70cb60a9dc4b2
renbz <1362206371@qq.com>
2022-05-22 22:35:33 +0800

    每日一题  05_22_0464_我能赢吗

---
+++ 12_LeetCode/src/每日一题_2022_05月/R05_22_0464_我能赢吗.java
@@ -0,0 +1,43 @@
+package 每日一题_2022_05月;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/5/22 21:45
+ * @Description:
+ */
+
+public class R05_22_0464_我能赢吗 {
+
+    Map<Integer, Boolean> memo = new HashMap<Integer, Boolean>();
+
+    public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+        if ((1 + maxChoosableInteger) * (maxChoosableInteger) / 2 < desiredTotal) {
+            return false;
+        }
+        return dfs(maxChoosableInteger, 0, desiredTotal, 0);
+    }
+
+    public boolean dfs(int maxChoosableInteger, int usedNumbers, int desiredTotal, int currentTotal) {
+        if (!memo.containsKey(usedNumbers)) {
+            boolean res = false;
+            for (int i = 0; i < maxChoosableInteger; i++) {
+                if (((usedNumbers >> i) & 1) == 0) {
+                    if (i + 1 + currentTotal >= desiredTotal) {
+                        res = true;
+                        break;
+                    }
+                    if (!dfs(maxChoosableInteger, usedNumbers | (1 << i), desiredTotal, currentTotal + i + 1)) {
+                        res = true;
+                        break;
+                    }
+                }
+            }
+            memo.put(usedNumbers, res);
+        }
+        return memo.get(usedNumbers);
+    }
+
+}
