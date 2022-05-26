e729526d87cd3feac051a3033467bd272150698c
renbz <1362206371@qq.com>
2022-05-26 16:15:53 +0800

    每日一题  05_26_0699_掉落的方块

---
+++ 12_LeetCode/src/每日一题_2022_05月/R05_26_0699_掉落的方块.java
@@ -0,0 +1,33 @@
+package 每日一题_2022_05月;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/5/26 14:48
+ * @Description:
+ */
+
+public class R05_26_0699_掉落的方块 {
+
+    public List<Integer> fallingSquares(int[][] positions) {
+        int n = positions.length;
+        List<Integer> heights = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            int left1 = positions[i][0], right1 = positions[i][0] + positions[i][1] - 1;
+            int height = positions[i][1];
+            for (int j = 0; j < i; j++) {
+                int left2 = positions[j][0], right2 = positions[j][0] + positions[j][1] - 1;
+                if (right1 >= left2 && right2 >= left1) {
+                    height = Math.max(height, heights.get(j) + positions[i][1]);
+                }
+            }
+            heights.add(height);
+        }
+        for (int i = 1; i < n; i++) {
+            heights.set(i, Math.max(heights.get(i), heights.get(i - 1)));
+        }
+        return heights;
+    }
+}
