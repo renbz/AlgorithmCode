35e111435fdd21141e95ce6ad6432f1b65557c98
renbz <1362206371@qq.com>
2022-05-31 12:28:05 +0800

    每日一题  05_31_剑指_Offer_II_114_外星文字典

---
+++ 12_LeetCode/src/每日一题_2022_05月/R05_31_剑指_Offer_II_114_外星文字典.java
@@ -0,0 +1,82 @@
+package 每日一题_2022_05月;
+
+import java.util.*;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/5/31 12:24
+ * @Description:
+ */
+
+public class R05_31_剑指_Offer_II_114_外星文字典 {
+
+    static final int VISITING = 1, VISITED = 2;
+    Map<Character, List<Character>> edges = new HashMap<Character, List<Character>>();
+    Map<Character, Integer> states = new HashMap<Character, Integer>();
+    boolean valid = true;
+    char[] order;
+    int index;
+
+    public String alienOrder(String[] words) {
+        int length = words.length;
+        for (String word : words) {
+            int wordLength = word.length();
+            for (int j = 0; j < wordLength; j++) {
+                char c = word.charAt(j);
+                edges.putIfAbsent(c, new ArrayList<Character>());
+            }
+        }
+        for (int i = 1; i < length && valid; i++) {
+            addEdge(words[i - 1], words[i]);
+        }
+        order = new char[edges.size()];
+        index = edges.size() - 1;
+        Set<Character> letterSet = edges.keySet();
+        for (char u : letterSet) {
+            if (!states.containsKey(u)) {
+                dfs(u);
+            }
+        }
+        if (!valid) {
+            return "";
+        }
+        return new String(order);
+    }
+
+    public void addEdge(String before, String after) {
+        int length1 = before.length(), length2 = after.length();
+        int length = Math.min(length1, length2);
+        int index = 0;
+        while (index < length) {
+            char c1 = before.charAt(index), c2 = after.charAt(index);
+            if (c1 != c2) {
+                edges.get(c1).add(c2);
+                break;
+            }
+            index++;
+        }
+        if (index == length && length1 > length2) {
+            valid = false;
+        }
+    }
+
+    public void dfs(char u) {
+        states.put(u, VISITING);
+        List<Character> adjacent = edges.get(u);
+        for (char v : adjacent) {
+            if (!states.containsKey(v)) {
+                dfs(v);
+                if (!valid) {
+                    return;
+                }
+            } else if (states.get(v) == VISITING) {
+                valid = false;
+                return;
+            }
+        }
+        states.put(u, VISITED);
+        order[index] = u;
+        index--;
+    }
+
+}
