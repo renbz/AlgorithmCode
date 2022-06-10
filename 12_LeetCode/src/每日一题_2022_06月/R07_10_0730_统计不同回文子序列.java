ca69c750c142c692ee8d500314f97129e5da09d4
renbz <1362206371@qq.com>
2022-06-10 23:03:46 +0800

    每日一题  07_10_0730_统计不同回文子序列

---
+++ 12_LeetCode/src/每日一题_2022_06月/R07_10_0730_统计不同回文子序列.java
@@ -0,0 +1,44 @@
+package 每日一题_2022_06月;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/6/10 23:02
+ * @Description:
+ */
+
+public class R07_10_0730_统计不同回文子序列 {
+
+    public int countPalindromicSubsequences(String s) {
+        final int MOD = 1000000007;
+        int n = s.length();
+        int[][][] dp = new int[4][n][n];
+        for (int i = 0; i < n; i++) {
+            dp[s.charAt(i) - 'a'][i][i] = 1;
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                int j = i + len - 1;
+                for (char c = 'a'; c <= 'd'; c++) {
+                    int k = c - 'a';
+                    if (s.charAt(i) == c && s.charAt(j) == c) {
+                        dp[k][i][j] = (2 + (dp[0][i + 1][j - 1] + dp[1][i + 1][j - 1]) % MOD + (dp[2][i + 1][j - 1] + dp[3][i + 1][j - 1]) % MOD) % MOD;
+                    } else if (s.charAt(i) == c) {
+                        dp[k][i][j] = dp[k][i][j - 1];
+                    } else if (s.charAt(j) == c) {
+                        dp[k][i][j] = dp[k][i + 1][j];
+                    } else {
+                        dp[k][i][j] = dp[k][i + 1][j - 1];
+                    }
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < 4; i++) {
+            res = (res + dp[i][0][n - 1]) % MOD;
+        }
+        return res;
+    }
+
+}
