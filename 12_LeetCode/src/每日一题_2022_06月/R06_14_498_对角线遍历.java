c960432d5ddbc97907068a3d41180e0b7caa0fcd
renbz <1362206371@qq.com>
2022-06-15 21:29:46 +0800

    每日一题  06_14_498_对角线遍历

---
+++ 12_LeetCode/src/每日一题_2022_06月/R06_14_498_对角线遍历.java
@@ -0,0 +1,40 @@
+package 每日一题_2022_06月;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/6/15 21:27
+ * @Description:
+ */
+
+public class R06_14_498_对角线遍历 {
+
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] res = new int[m * n];
+        int pos = 0;
+        for (int i = 0; i < m + n - 1; i++) {
+            if (i % 2 == 1) {
+                int x = i < n ? 0 : i - n + 1;
+                int y = i < n ? i : n - 1;
+                while (x < m && y >= 0) {
+                    res[pos] = mat[x][y];
+                    pos++;
+                    x++;
+                    y--;
+                }
+            } else {
+                int x = i < m ? i : m - 1;
+                int y = i < m ? 0 : i - m + 1;
+                while (x >= 0 && y < n) {
+                    res[pos] = mat[x][y];
+                    pos++;
+                    x--;
+                    y++;
+                }
+            }
+        }
+        return res;
+    }
+
+}
