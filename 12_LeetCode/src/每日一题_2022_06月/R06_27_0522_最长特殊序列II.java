2c6c9add9debfc5f22b69dd779a02dde1ddfb968
renbz <1362206371@qq.com>
2022-06-28 19:59:16 +0800

    每日一题  06_27_0522_最长特殊序列II

---
+++ 12_LeetCode/src/每日一题_2022_06月/R06_27_0522_最长特殊序列II.java
@@ -0,0 +1,40 @@
+package 每日一题_2022_06月;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/6/28 19:58
+ * @Description:
+ */
+
+public class R06_27_0522_最长特殊序列II {
+
+    public int findLUSlength(String[] strs) {
+        int n = strs.length;
+        int ans = -1;
+        for (int i = 0; i < n; ++i) {
+            boolean check = true;
+            for (int j = 0; j < n; ++j) {
+                if (i != j && isSubseq(strs[i], strs[j])) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                ans = Math.max(ans, strs[i].length());
+            }
+        }
+        return ans;
+    }
+
+    public boolean isSubseq(String s, String t) {
+        int ptS = 0, ptT = 0;
+        while (ptS < s.length() && ptT < t.length()) {
+            if (s.charAt(ptS) == t.charAt(ptT)) {
+                ++ptS;
+            }
+            ++ptT;
+        }
+        return ptS == s.length();
+    }
+
+}
