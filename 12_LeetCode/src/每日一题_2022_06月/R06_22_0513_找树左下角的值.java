359400d39d5bfa7982c8cc4c71022d88b0ae50ea
renbz <1362206371@qq.com>
2022-06-28 14:52:38 +0800

    每日一题  06_22_0513_找树左下角的值

---
+++ 12_LeetCode/src/每日一题_2022_06月/R06_22_0513_找树左下角的值.java
@@ -0,0 +1,53 @@
+package 每日一题_2022_06月;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/6/28 14:51
+ * @Description:
+ */
+
+public class R06_22_0513_找树左下角的值 {
+
+    int curVal = 0;
+    int curHeight = 0;
+
+    public int findBottomLeftValue(TreeNode root) {
+        int curHeight = 0;
+        dfs(root, 0);
+        return curVal;
+    }
+
+    public void dfs(TreeNode root, int height) {
+        if (root == null) {
+            return;
+        }
+        height++;
+        dfs(root.left, height);
+        dfs(root.right, height);
+        if (height > curHeight) {
+            curHeight = height;
+            curVal = root.val;
+        }
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
