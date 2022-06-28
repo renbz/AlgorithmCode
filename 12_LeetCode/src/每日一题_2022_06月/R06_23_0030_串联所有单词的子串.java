11d6f5fb9d4a7badc957c80ea51e5fff0aa3d0eb
renbz <1362206371@qq.com>
2022-06-28 14:55:07 +0800

    每日一题  06_23_0030_串联所有单词的子串

---
+++ 12_LeetCode/src/每日一题_2022_06月/R06_23_0030_串联所有单词的子串.java
@@ -0,0 +1,55 @@
+package 每日一题_2022_06月;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/6/28 14:54
+ * @Description:
+ */
+
+public class R06_23_0030_串联所有单词的子串 {
+
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList<Integer>();
+        int m = words.length, n = words[0].length(), ls = s.length();
+        for (int i = 0; i < n; i++) {
+            if (i + m * n > ls) {
+                break;
+            }
+            Map<String, Integer> differ = new HashMap<String, Integer>();
+            for (int j = 0; j < m; j++) {
+                String word = s.substring(i + j * n, i + (j + 1) * n);
+                differ.put(word, differ.getOrDefault(word, 0) + 1);
+            }
+            for (String word : words) {
+                differ.put(word, differ.getOrDefault(word, 0) - 1);
+                if (differ.get(word) == 0) {
+                    differ.remove(word);
+                }
+            }
+            for (int start = i; start < ls - m * n + 1; start += n) {
+                if (start != i) {
+                    String word = s.substring(start + (m - 1) * n, start + m * n);
+                    differ.put(word, differ.getOrDefault(word, 0) + 1);
+                    if (differ.get(word) == 0) {
+                        differ.remove(word);
+                    }
+                    word = s.substring(start - n, start);
+                    differ.put(word, differ.getOrDefault(word, 0) - 1);
+                    if (differ.get(word) == 0) {
+                        differ.remove(word);
+                    }
+                }
+                if (differ.isEmpty()) {
+                    res.add(start);
+                }
+            }
+        }
+        return res;
+    }
+
+}
