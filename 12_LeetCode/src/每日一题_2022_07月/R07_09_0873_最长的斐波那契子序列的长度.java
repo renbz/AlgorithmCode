2e57e152e68c7dd5ee7791c05996dc3102fc4cf1
renbz <1362206371@qq.com>
2022-07-10 13:27:34 +0800

    每日一题  07_09_0873_最长的斐波那契子序列的长度

---
+++ 12_LeetCode/src/每日一题_2022_07月/R07_09_0873_最长的斐波那契子序列的长度.java
@@ -0,0 +1,33 @@
+package 每日一题_2022_07月;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/7/9 23:55
+ * @Description:
+ */
+
+public class R07_09_0873_最长的斐波那契子序列的长度 {
+
+    public int lenLongestFibSubseq(int[] arr) {
+        Map<Integer, Integer> indices = new HashMap<Integer, Integer>();
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            indices.put(arr[i], i);
+        }
+        int[][] dp = new int[n][n];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] * 2 > arr[i]; j--) {
+                int k = indices.getOrDefault(arr[i] - arr[j], -1);
+                if (k >= 0) {
+                    dp[j][i] = Math.max(dp[k][j] + 1, 3);
+                }
+                ans = Math.max(ans, dp[j][i]);
+            }
+        }
+        return ans;
+    }
+}
