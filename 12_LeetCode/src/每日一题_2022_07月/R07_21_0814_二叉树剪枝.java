c515d571b2abdd4990a4ce49f750386a3c405d9b
renbz <1362206371@qq.com>
2022-07-22 23:34:02 +0800

    每日一题  07_21_0814_二叉树剪枝

---
+++ 12_LeetCode/src/每日一题_2022_07月/R07_21_0814_二叉树剪枝.java
@@ -0,0 +1,40 @@
+package 每日一题_2022_07月;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/7/21 21:37
+ * @Description:
+ */
+
+public class R07_21_0814_二叉树剪枝 {
+
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.left == null && root.right == null && root.val == 0) {
+            return null;
+        }
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
