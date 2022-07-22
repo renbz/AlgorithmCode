ae976084791f1d136416c0e09f24376a6f761da0
renbz <1362206371@qq.com>
2022-07-22 23:35:55 +0800

    每日一题  07_22_0757_设置交集大小至少为2

---
+++ 12_LeetCode/src/每日一题_2022_07月/R07_22_0757_设置交集大小至少为2.java
@@ -0,0 +1,47 @@
+package 每日一题_2022_07月;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/7/22 23:34
+ * @Description:
+ */
+
+public class R07_22_0757_设置交集大小至少为2 {
+
+    public int intersectionSizeTwo(int[][] intervals) {
+        int n = intervals.length;
+        int res = 0;
+        int m = 2;
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            }
+            return a[0] - b[0];
+        });
+        List<Integer>[] temp = new List[n];
+        for (int i = 0; i < n; i++) {
+            temp[i] = new ArrayList<Integer>();
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = intervals[i][0], k = temp[i].size(); k < m; j++, k++) {
+                res++;
+                help(intervals, temp, i - 1, j);
+            }
+        }
+        return res;
+    }
+
+    public void help(int[][] intervals, List<Integer>[] temp, int pos, int num) {
+        for (int i = pos; i >= 0; i--) {
+            if (intervals[i][1] < num) {
+                break;
+            }
+            temp[i].add(num);
+        }
+    }
+
+}
