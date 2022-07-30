8c589e22105a979bfffcdc8b83778317492308cd
renbz <1362206371@qq.com>
2022-07-30 23:30:47 +0800

    每日一题  07_30_0952_按公因数计算最大组件大小

---
+++ 12_LeetCode/src/每日一题_2022_07月/R07_30_0952_按公因数计算最大组件大小.java
@@ -0,0 +1,69 @@
+package 每日一题_2022_07月;
+
+import java.util.Arrays;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/7/30 23:29
+ * @Description:
+ */
+
+public class R07_30_0952_按公因数计算最大组件大小 {
+
+    public int largestComponentSize(int[] nums) {
+        int m = Arrays.stream(nums).max().getAsInt();
+        UnionFind uf = new UnionFind(m + 1);
+        for (int num : nums) {
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) {
+                    uf.union(num, i);
+                    uf.union(num, num / i);
+                }
+            }
+        }
+        int[] counts = new int[m + 1];
+        int ans = 0;
+        for (int num : nums) {
+            int root = uf.find(num);
+            counts[root]++;
+            ans = Math.max(ans, counts[root]);
+        }
+        return ans;
+    }
+}
+
+class UnionFind {
+    int[] parent;
+    int[] rank;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        rank = new int[n];
+    }
+
+    public void union(int x, int y) {
+        int rootx = find(x);
+        int rooty = find(y);
+        if (rootx != rooty) {
+            if (rank[rootx] > rank[rooty]) {
+                parent[rooty] = rootx;
+            } else if (rank[rootx] < rank[rooty]) {
+                parent[rootx] = rooty;
+            } else {
+                parent[rooty] = rootx;
+                rank[rootx]++;
+            }
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+}
