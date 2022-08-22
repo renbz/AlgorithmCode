e893c3d691e5c72073d338cb85149e402801f1a9
renbz <1362206371@qq.com>
2022-08-22 22:47:01 +0800

    每日一题  08_13_0768_最多能完成排序的块II

---
+++ 12_LeetCode/src/每日一题_2022_08月/R08_13_0768_最多能完成排序的块II.java
@@ -0,0 +1,30 @@
+package 每日一题_2022_08月;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/8/22 22:46
+ * @Description:
+ */
+
+public class R08_13_0768_最多能完成排序的块II {
+
+    public int maxChunksToSorted(int[] arr) {
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        for (int num : arr) {
+            if (stack.isEmpty() || num >= stack.peek()) {
+                stack.push(num);
+            } else {
+                int mx = stack.pop();
+                while (!stack.isEmpty() && stack.peek() > num) {
+                    stack.pop();
+                }
+                stack.push(mx);
+            }
+        }
+        return stack.size();
+    }
+
+}
