88b2977bd11644952ff4b8e28e9ad1cbd11a6a0b
renbz <1362206371@qq.com>
2022-08-22 22:37:16 +0800

    每日一题  08_07_0636_函数的独占时间

---
+++ 12_LeetCode/src/每日一题_2022_08月/R08_07_0636_函数的独占时间.java
@@ -0,0 +1,39 @@
+package 每日一题_2022_08月;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/8/22 22:36
+ * @Description:
+ */
+
+public class R08_07_0636_函数的独占时间 {
+
+    public int[] exclusiveTime(int n, List<String> logs) {
+        Deque<int[]> stack = new ArrayDeque<int[]>(); // {idx, 开始运行的时间}
+        int[] res = new int[n];
+        for (String log : logs) {
+            int idx = Integer.parseInt(log.substring(0, log.indexOf(':')));
+            String type = log.substring(log.indexOf(':') + 1, log.lastIndexOf(':'));
+            int timestamp = Integer.parseInt(log.substring(log.lastIndexOf(':') + 1));
+            if ("start".equals(type)) {
+                if (!stack.isEmpty()) {
+                    res[stack.peek()[0]] += timestamp - stack.peek()[1];
+                    stack.peek()[1] = timestamp;
+                }
+                stack.push(new int[]{idx, timestamp});
+            } else {
+                int[] t = stack.pop();
+                res[t[0]] += timestamp - t[1] + 1;
+                if (!stack.isEmpty()) {
+                    stack.peek()[1] = timestamp + 1;
+                }
+            }
+        }
+        return res;
+    }
+
+}
