3f80cb5caeea6d75a8efc3e0f5ab5ff666b22d14
renbz <1362206371@qq.com>
2022-08-22 22:33:34 +0800

    每日一题  08_05_0623_在二叉树中增加一行

---
+++ 12_LeetCode/src/每日一题_2022_08月/R08_05_0623_在二叉树中增加一行.java
@@ -0,0 +1,48 @@
+package 每日一题_2022_08月;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/8/22 22:32
+ * @Description:
+ */
+
+public class R08_05_0623_在二叉树中增加一行 {
+
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (root == null) {
+            return null;
+        }
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
+        }
+        if (depth == 2) {
+            root.left = new TreeNode(val, root.left, null);
+            root.right = new TreeNode(val, null, root.right);
+        } else {
+            root.left = addOneRow(root.left, val, depth - 1);
+            root.right = addOneRow(root.right, val, depth - 1);
+        }
+        return root;
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
