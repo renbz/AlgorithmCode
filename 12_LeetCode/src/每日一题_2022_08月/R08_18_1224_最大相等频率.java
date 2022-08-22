a66303a6f76532280b77c0baac11ab2d7cef9cb3
renbz <1362206371@qq.com>
2022-08-22 22:55:10 +0800

    每日一题  08_18_1224_最大相等频率

---
+++ 12_LeetCode/src/每日一题_2022_08月/R08_18_1224_最大相等频率.java
@@ -0,0 +1,35 @@
+package 每日一题_2022_08月;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/8/22 22:54
+ * @Description:
+ */
+
+public class R08_18_1224_最大相等频率 {
+
+    public int maxEqualFreq(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+        int res = 0, maxFreq = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (count.getOrDefault(nums[i], 0) > 0) {
+                freq.put(count.get(nums[i]), freq.get(count.get(nums[i])) - 1);
+            }
+            count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
+            maxFreq = Math.max(maxFreq, count.get(nums[i]));
+            freq.put(count.get(nums[i]), freq.getOrDefault(count.get(nums[i]), 0) + 1);
+            boolean ok = maxFreq == 1 ||
+                    freq.get(maxFreq) * maxFreq + freq.get(maxFreq - 1) * (maxFreq - 1) == i + 1 && freq.get(maxFreq) == 1 ||
+                    freq.get(maxFreq) * maxFreq + 1 == i + 1 && freq.get(1) == 1;
+            if (ok) {
+                res = Math.max(res, i + 1);
+            }
+        }
+        return res;
+    }
+
+}
