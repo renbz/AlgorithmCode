2efb64588e76ccdf9478a1195f6604fa644eb6c3
renbz <1362206371@qq.com>
2022-09-07 21:23:32 +0800

    每日一题  09_07_1592_重新排列单词间的空格

---
+++ 12_LeetCode/src/每日一题_2022_09月/R09_07_1592_重新排列单词间的空格.java
@@ -0,0 +1,42 @@
+package 每日一题_2022_09月;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/9/7 19:53
+ * @Description:
+ */
+
+public class R09_07_1592_重新排列单词间的空格 {
+
+    public String reorderSpaces(String text) {
+        int length = text.length();
+        String[] words = text.trim().split("\\s+");
+        int cntSpace = length;
+        for (String word : words) {
+            cntSpace -= word.length();
+        }
+        StringBuilder sb = new StringBuilder();
+        if (words.length == 1) {
+            sb.append(words[0]);
+            for (int i = 0; i < cntSpace; i++) {
+                sb.append(' ');
+            }
+            return sb.toString();
+        }
+        int perSpace = cntSpace / (words.length - 1);
+        int restSpace = cntSpace % (words.length - 1);
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0) {
+                for (int j = 0; j < perSpace; j++) {
+                    sb.append(' ');
+                }
+            }
+            sb.append(words[i]);
+        }
+        for (int i = 0; i < restSpace; i++) {
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
+
+}
