8a2f3b41960aad91e1fc5410bb8c4a5d7ecc3799
renbz <1362206371@qq.com>
2022-09-11 17:02:05 +0800

    每日一题  09_10_0669_修剪二叉搜索树

---
+++ 12_LeetCode/src/每日一题_2022_09月/R09_10_0669_修剪二叉搜索树.java
@@ -0,0 +1,45 @@
+package 每日一题_2022_09月;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/9/10 21:39
+ * @Description:
+ */
+
+public class R09_10_0669_修剪二叉搜索树 {
+
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        } else if (root.val > high) {
+            return trimBST(root.left, low, high);
+        } else {
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+            return root;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
