5522598f46a680fc79a24719c4c8bda2ff60b094
renbz <1362206371@qq.com>
2022-09-14 20:27:10 +0800

    每日一题  09_13_0670_最大交换

---
+++ 12_LeetCode/src/每日一题_2022_09月/R09_13_0670_最大交换.java
@@ -0,0 +1,30 @@
+package 每日一题_2022_09月;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/9/13 22:02
+ * @Description:
+ */
+
+public class R09_13_0670_最大交换 {
+
+    public int maximumSwap(int num) {
+        char[] charArray = String.valueOf(num).toCharArray();
+        int n = charArray.length;
+        int maxNum = num;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(charArray, i, j);
+                maxNum = Math.max(maxNum, Integer.parseInt(new String(charArray)));
+                swap(charArray, i, j);
+            }
+        }
+        return maxNum;
+    }
+
+    public void swap(char[] charArray, int i, int j) {
+        char temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+    }
+}
