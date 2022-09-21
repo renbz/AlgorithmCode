979d985bfb0f7c3e7dd52b42adc979e47de5e279
renbz <1362206371@qq.com>
2022-09-21 20:56:42 +0800

    每日一题  09_20_0698_划分为k个相等的子集

---
+++ 12_LeetCode/src/每日一题_2022_09月/R09_20_0698_划分为k个相等的子集.java
@@ -0,0 +1,54 @@
+package 每日一题_2022_09月;
+
+import java.util.Arrays;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/9/20 20:12
+ * @Description:
+ */
+
+public class R09_20_0698_划分为k个相等的子集 {
+
+    int[] nums;
+    int per, n;
+    boolean[] dp;
+
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        this.nums = nums;
+        int all = Arrays.stream(nums).sum();
+        if (all % k != 0) {
+            return false;
+        }
+        per = all / k;
+        Arrays.sort(nums);
+        n = nums.length;
+        if (nums[n - 1] > per) {
+            return false;
+        }
+        dp = new boolean[1 << n];
+        Arrays.fill(dp, true);
+        return dfs((1 << n) - 1, 0);
+    }
+
+    public boolean dfs(int s, int p) {
+        if (s == 0) {
+            return true;
+        }
+        if (!dp[s]) {
+            return dp[s];
+        }
+        dp[s] = false;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] + p > per) {
+                break;
+            }
+            if (((s >> i) & 1) != 0) {
+                if (dfs(s ^ (1 << i), (p + nums[i]) % per)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
