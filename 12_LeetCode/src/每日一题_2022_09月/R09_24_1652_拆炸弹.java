74b5878e691584f5afa5c7bf808fc3cf8cbf36d5
renbz <1362206371@qq.com>
2022-09-24 00:58:00 +0800

    每日一题  09_24_1652_拆炸弹

---
+++ 12_LeetCode/src/每日一题_2022_09月/R09_24_1652_拆炸弹.java
@@ -0,0 +1,37 @@
+package 每日一题_2022_09月;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/9/24 00:56
+ * @Description:
+ */
+
+public class R09_24_1652_拆炸弹 {
+
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        if (k == 0) {
+            return new int[n];
+        }
+        int[] res = new int[n];
+        int[] newCode = new int[n * 2];
+        System.arraycopy(code, 0, newCode, 0, n);
+        System.arraycopy(code, 0, newCode, n, n);
+        code = newCode;
+        int l = k > 0 ? 1 : n + k;
+        int r = k > 0 ? k : n - 1;
+        int w = 0;
+        for (int i = l; i <= r; i++) {
+            w += code[i];
+        }
+        for (int i = 0; i < n; i++) {
+            res[i] = w;
+            w -= code[l];
+            w += code[r + 1];
+            l++;
+            r++;
+        }
+        return res;
+    }
+
+}
