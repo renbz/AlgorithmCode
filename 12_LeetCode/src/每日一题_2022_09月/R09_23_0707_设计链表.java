df9c0ff60e8f75c81f4cd4fbac5b81a6e8be71a3
renbz <1362206371@qq.com>
2022-09-24 00:54:35 +0800

    每日一题  09_23_0707_设计链表

---
+++ 12_LeetCode/src/每日一题_2022_09月/R09_23_0707_设计链表.java
@@ -0,0 +1,75 @@
+package 每日一题_2022_09月;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/9/24 00:50
+ * @Description:
+ */
+
+public class R09_23_0707_设计链表 {
+
+
+    int size;
+    ListNode head;
+
+    public R09_23_0707_设计链表() {
+        size = 0;
+        head = new ListNode(0);
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+        ListNode cur = head;
+        for (int i = 0; i <= index; i++) {
+            cur = cur.next;
+        }
+        return cur.val;
+    }
+
+    public void addAtHead(int val) {
+        addAtIndex(0, val);
+    }
+
+    public void addAtTail(int val) {
+        addAtIndex(size, val);
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index > size) {
+            return;
+        }
+        index = Math.max(0, index);
+        size++;
+        ListNode pred = head;
+        for (int i = 0; i < index; i++) {
+            pred = pred.next;
+        }
+        ListNode toAdd = new ListNode(val);
+        toAdd.next = pred.next;
+        pred.next = toAdd;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            return;
+        }
+        size--;
+        ListNode pred = head;
+        for (int i = 0; i < index; i++) {
+            pred = pred.next;
+        }
+        pred.next = pred.next.next;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+}
