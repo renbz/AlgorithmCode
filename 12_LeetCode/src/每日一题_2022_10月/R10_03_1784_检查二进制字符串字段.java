06b1a90f58bf42adc33a8ecbc4d1a63acc379795
renbz <1362206371@qq.com>
2022-10-07 19:57:52 +0800

    每日一题  10_03_1784_检查二进制字符串字段

---
+++ 12_LeetCode/src/每日一题_2022_10月/R10_03_1784_检查二进制字符串字段.java
@@ -0,0 +1,15 @@
+package 每日一题_2022_10月;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/10/7 19:57
+ * @Description:
+ */
+
+public class R10_03_1784_检查二进制字符串字段 {
+
+    public boolean checkOnesSegment(String s) {
+        return !s.contains("01");
+    }
+
+}
