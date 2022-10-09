f592490e2263a59b067880bd50573187ceb71896
renbz <1362206371@qq.com>
2022-10-09 18:58:18 +0800

    每日一题  10_08_0870_优势洗牌

---
+++ 12_LeetCode/src/每日一题_2022_10月/R10_08_0870_优势洗牌.java
@@ -0,0 +1,38 @@
+package 每日一题_2022_10月;
+
+import java.util.Arrays;
+
+/**
+ * @author Ren - 乔治的哥们
+ * @Date: 2022/10/8 21:04
+ * @Description:
+ */
+
+public class R10_08_0870_优势洗牌 {
+
+    public int[] advantageCount(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        Integer[] idx1 = new Integer[n];
+        Integer[] idx2 = new Integer[n];
+        for (int i = 0; i < n; ++i) {
+            idx1[i] = i;
+            idx2[i] = i;
+        }
+        Arrays.sort(idx1, (i, j) -> nums1[i] - nums1[j]);
+        Arrays.sort(idx2, (i, j) -> nums2[i] - nums2[j]);
+
+        int[] ans = new int[n];
+        int left = 0, right = n - 1;
+        for (int i = 0; i < n; ++i) {
+            if (nums1[idx1[i]] > nums2[idx2[left]]) {
+                ans[idx2[left]] = nums1[idx1[i]];
+                ++left;
+            } else {
+                ans[idx2[right]] = nums1[idx1[i]];
+                --right;
+            }
+        }
+        return ans;
+    }
+
+}
